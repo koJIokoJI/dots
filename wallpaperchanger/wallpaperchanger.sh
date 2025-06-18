e134bacf3792c4950eb48f44b890ebc66810e50a
mia <mia@beobachter.com>
2025-06-18 16:05:39 +0000

    pushed dots

---
+++ wallpaperchanger/wallpaperchanger.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wallpaper=$(find ~/Pictures/Wallpaper/ -type f -name "*.jpg" -o -name "*.png" -o -name "*.gif" | shuf -n 1)
+
+export WALLPAPER=~/Pictures/Wallpaper/$wallpaper
+
+
+swww img $wallpaper --transition-type random --transition-step 100 --transition-fps 60 --transition-duration .3
+
+wal -c
+wal -i $wallpaper
+
+pywalfox update
+
+#~/.config/swaync/refresh.sh
+
+swaync-client --reload-css
+
+walogram
+
+killall -SIGUSR2 waybar &
+
+rm -rf $HOME/.cache/swww
