5a50b686ba58156b5322fc0a77ce00e39a0039e2
mia <mxssedxnactxon@gmail.com>
2025-06-19 10:56:13 +0000

    add wallpaper to hyprlock

--- .config/wallpaperchanger/wallpaperchanger.sh
+++ .config/wallpaperchanger/wallpaperchanger.sh
@@ -2,7 +2,8 @@
 
 wallpaper=$(find ~/Pictures/Wallpaper/ -type f -name "*.jpg" -o -name "*.png" -o -name "*.gif" | shuf -n 1)
 
-export WALLPAPER=~/Pictures/Wallpaper/$wallpaper
+rm ~/.config/wallpaperchanger/current_wallpaper.conf
+echo '$WALLPAPER' =  $wallpaper >> ~/.config/wallpaperchanger/current_wallpaper.conf
 
 
 swww img $wallpaper --transition-type random --transition-step 100 --transition-fps 60 --transition-duration .3
